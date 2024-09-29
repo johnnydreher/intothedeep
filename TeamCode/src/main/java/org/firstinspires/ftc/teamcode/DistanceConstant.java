fe655fe5a20749e34364bdff0c38f1d6b4244ea0
Johnny Dreher <johnny.dreher@teksea.net>
2024-09-29 17:49:02 -0300

    Drivetrain testado

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceConstant.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@Config
+public class DistanceConstant {
+    public static double distanceX = 100;
+    public static double distanceY = 100;
+    public static double angle = 90;
+
+}
+
+
+
+
